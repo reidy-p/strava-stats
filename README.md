d644ee3ac088195417823ccf122e367c2e8c1eff
Paul Reidy <paul_reidy@outlook.com>
2019-08-03 18:24:48 +0100

    Change to sqlite database

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
+[Strava](https://www.strava.com) is a popular social network where runners and cyclists can upload and compare data from their activities. However, athletic performance can be significantly affected by weather conditions such as temperature, wind, and humidity and this data is typically not available on Strava. This project allows a user to download their own running data from Strava's API and to combine it with weather data from the [DarkSky API](https://www.darksky.net/dev/) to adjust for weather conditions.
+
+This project is still under active development.
+
+Features
+---
+
+Usage
+---
+
+
 TODO
 ---
 * Top 10 activities with the biggest discrepancies between actual speed and adjusted speed
 * Do something about rate limits? Only update those activities that are not already there? (use after certain date that is read in) 
-* Command line argument to collect versus analyse
-* Write out as JSON?
+* Command line arguments
 * Adjusting pacing depending on wind (maybe too hard because of direction)
 * Use richer python types for quantities rather than the simple string or numeric values that the Strava API returns or the simple quantities that stravalib returns
 * Grouping similar activities. Is this done already or is it something I could do?
+* Check all activities are present
+
 
--- strava_access.py
+++ strava_access.py
@@ -9,6 +9,8 @@ from pint import UnitRegistry
 import logging
 import yaml
 import sys
+import sqlite3
+from datetime import datetime
 
 
 class HandlerWithAuth(http.server.BaseHTTPRequestHandler):
@@ -22,41 +24,38 @@ class HandlerWithAuth(http.server.BaseHTTPRequestHandler):
             self.wfile.write(bytes(open("authorized.html").read(), "UTF-8"))
 
             token_response = client.exchange_code_for_token(client_id=secret_keys['client_id'], client_secret=secret_keys['client_secret'], code=parsed_qs['code'])
-            activities = client.get_activities()
 
-            with open('activities.csv', 'w', newline='') as csvfile:
-                activity_writer = csv.writer(csvfile, delimiter=',')
-                cols = ['upload_id', 'start_date', 'start_date_local', 'moving_time', 'type', 'total_elevation_gain', 'average_speed', 'distance', 'location_city', 
-                        'location_country', 'start_latitude', 'start_longitude', 'summary', 'temperature', 'humidity', 'wind_speed', 'wind_gust', 'hadley_score',
-                        'minutes_per_km', 'minutes_per_km_adjusted', 'pace_discrepancy']
-                activity_writer.writerow(cols)
-                ureg = UnitRegistry()
+            conn = sqlite3.connect('activities.db')
+            with conn:
+                cursor = conn.cursor()
+                create_table_if_not_exists(cursor)
+                latest_activity_date = get_latest_activity_date(cursor)
 
-                logger = logging.getLogger()
-                logger.setLevel('ERROR')
+            activities = client.get_activities(after=latest_activity_date)
 
-                for activity in activities:
-                    request = make_darksky_request(activity.start_latitude, activity.start_longitude, activity.start_date_local)
+            logger = logging.getLogger()
+            logger.setLevel('ERROR')
 
-                    if request.status_code == 200:
-                        weather_data = request.json()['currently']
+            for activity in activities:
+                darksky_request = make_darksky_request(activity.start_latitude, activity.start_longitude, activity.start_date_local)
 
-                        moving_minutes = (activity.moving_time.total_seconds() * ureg.seconds).to(ureg.minutes)
-                        km = (activity.distance.get_num() * ureg.meters).to(ureg.kilometers)
-                        minutes_per_km = moving_minutes / km
+                if darksky_request.status_code == 200:
+                    weather_data = darksky_request.json()['currently']
+
+                    with conn:
+                        c = conn.cursor()
 
                         (hadley_score, hadley_pace_adjustment) = calculate_hadley_score(weather_data['dewPoint'], weather_data['temperature'])
+                        minutes_per_km = calculate_speed(activity.moving_time.total_seconds(), activity.distance.get_num())
                         minutes_per_km_hadley_adjusted = minutes_per_km * (1 - hadley_pace_adjustment)
 
-                        activity_writer.writerow([
+                        row = (
                             activity.upload_id,
-                            activity.start_date,
-                            activity.start_date_local,
-                            activity.moving_time,
+                            activity.start_date.timestamp(),
+                            activity.moving_time.total_seconds(),
                             activity.type,
-                            activity.total_elevation_gain,
-                            activity.average_speed,
-                            activity.distance,
+                            activity.total_elevation_gain.get_num(),
+                            activity.distance.get_num(),
                             activity.location_city,
                             activity.location_country,
                             activity.start_latitude,
@@ -68,17 +67,27 @@ class HandlerWithAuth(http.server.BaseHTTPRequestHandler):
                             weather_data['windSpeed'],
                             weather_data['windGust'],
                             hadley_score,
-                            minutes_per_km,
-                            minutes_per_km_hadley_adjusted,
-                            abs(minutes_per_km - minutes_per_km_hadley_adjusted)
-                        ])
-                    elif request.status_code == 400:
-                        print("{} Error: {}".format(request.json()['code'], request.json()['error']))
-                        print("Skipping request and continuing to next activity")
-                    else:
-                        print("{} Error: {}".format(request.json()['code'], request.json()['error']))
-                        print("Terminating")
-                        break
+                            minutes_per_km.magnitude,
+                            minutes_per_km_hadley_adjusted.magnitude
+                        )
+
+                        # Will there be duplicate rows?
+                        # Avoid SQL injection
+                        c.execute("""INSERT OR IGNORE INTO activities
+                            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", row)
+
+
+                elif darksky_request.status_code == 400:
+                    error_msg = "{} Error: {}".format(darksky_request.json()['code'], darksky_request.json()['error'])
+                    print(error_msg + " Skipping request and continuing to next activity")
+                elif darksky_request.status_code == 403:
+                    print("{} Error: DarkSky API daily usage limit exceeded".format(darksky_request.json()['code']))
+                    print("Terminating")
+                    break
+                else:
+                    print("{} Error: {}".format(darksky_request.json()['code'], darksky_request.json()['error']))
+                    print("Terminating")
+                    break
 
         else:
             self.send_response(401)
@@ -93,11 +102,19 @@ class TCPServerWithReusableAddress(socketserver.TCPServer):
     # succession leads to an error
     allow_reuse_address = True
 
+
 def make_darksky_request(latitude, longitude, time):
     # Dark Sky API can't handle microseconds
     clean_time = time.replace(microsecond=0)
     url = "https://api.darksky.net/forecast/{}/{},{}".format(secret_keys['darksky_api_key'], latitude, longitude, clean_time)
     return requests.get(url)
+
+def calculate_speed(moving_time_seconds, distance_metres):
+    ureg = UnitRegistry()
+    moving_minutes = (moving_time_seconds * ureg.seconds).to(ureg.minutes)
+    km = (distance_metres * ureg.meters).to(ureg.kilometers)
+
+    return moving_minutes / km
 
 def calculate_hadley_score(dewPoint, temperature):
     hadley_score = dewPoint + temperature
@@ -125,27 +142,55 @@ def calculate_hadley_score(dewPoint, temperature):
 
     return (hadley_score, adjustment)
 
-if len(sys.argv) < 2:
-    print("You must pass command line arguments")
+def get_latest_activity_date(cursor):
+    cursor.execute("""SELECT MAX(start_date_unix) FROM activities""")
+    latest_activity_date = cursor.fetchone()[0]
+    if latest_activity_date is None:
+        return None
+    else:
+        print("latest date is", latest_activity_date)
+        return datetime.fromtimestamp(latest_activity_date)
 
-elif sys.argv[1] == "create_data":
+def create_table_if_not_exists(cursor):
+    sql_create_table = """CREATE TABLE IF NOT EXISTS activities (
+        upload_id INTEGER PRIMARY KEY,
+        start_date_unix INTEGER NOT NULL,
+        moving_time_seconds INTEGER NOT NULL,
+        type TEXT NOT NULL,
+        total_elevation_gain_metres REAL NOT NULL,
+        distance_metres REAL NOT NULL,
+        location_city TEXT,
+        location_country TEXT NOT NULL,
+        start_latitude REAL NOT NULL,
+        start_longitude REAL NOT NULL,
+        summary TEXT NOT NULL,
+        temperature REAL NOT NULL,
+        humidity REAL NOT NULL,
+        dew_point REAL NOT NULL,
+        wind_speed REAL NOT NULL,
+        wind_gust REAL NOT NULL,
+        hadley_score REAL NOT NULL,
+        minutes_per_km REAL NOT NULL,
+        minutes_per_km_adjusted REAL NOT NULL
+    );
+    """
+    cursor.execute(sql_create_table)
 
-    with open("keys.yml", 'r') as keys_file:
-        secret_keys = yaml.safe_load(keys_file)
-    
-    client = Client()
-    authorize_url = client.authorization_url(
-        client_id=secret_keys['client_id'], 
-        redirect_uri='http://127.0.0.1:5000/authorized'
-    )
-    
-    # Have the user click the authorization URL, a 'code' param will be added to the redirect_uri
-    webbrowser.open(authorize_url)
-    
-    with TCPServerWithReusableAddress(("127.0.0.1", 5000), HandlerWithAuth) as httpd:
-        print("serving at port", 5000)
-        httpd.handle_request()
-        print("shutting down server")
-    
-else:
-    pass
+
+with open("keys.yml", 'r') as keys_file:
+    secret_keys = yaml.safe_load(keys_file)
+
+client = Client()
+authorize_url = client.authorization_url(
+    client_id=secret_keys['client_id'], 
+    redirect_uri='http://127.0.0.1:5000/authorized'
+)
+
+# Have the user click the authorization URL, a 'code' param will be added to the redirect_uri
+webbrowser.open(authorize_url)
+
+with TCPServerWithReusableAddress(("127.0.0.1", 5000), HandlerWithAuth) as httpd:
+    print("serving at port", 5000)
+    httpd.handle_request()
+    print("shutting down server")
+
